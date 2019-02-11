ac38cee4025b838ba11a93de5b5a6e38051a98f7
yc <564211612@qq.com>
2019-02-11 18:29:28 +0800

    22

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String say(){
-        return "hello china111";
+        return "hello china2";
     }
 }
