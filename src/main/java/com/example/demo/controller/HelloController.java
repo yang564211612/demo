cbefffa16b8f2d1e7530b2905ad6d475be917532
yc <564211612@qq.com>
2019-02-11 16:37:02 +0800

    部署启动文件

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String say(){
-        return "hello china";
+        return "hello china111";
     }
 }
